bb67284ddca8650a21d17a7bb30b0cf121781c8b
안재우 <ahnjw9114@kaist.ac.kr>
2024-08-07 11:07:51 +0900

    1622

--- 08/ahnjaewoo/1622.cpp
+++ 08/ahnjaewoo/1622.cpp
@@ -1,36 +1,51 @@
 class Fancy {
 private:
     vector<int> x;
+		vector<tuple<int, string, int>> operations;
+		// index, operation, value
+		int curIdx;
+		vector<tuple<int, int>> memo;
+		// current calculated index, current value
 public:
     int MAX = pow(10, 9) + 7;
 
     Fancy() {
         x.clear();
+				curIdx = -1;
     }
     
     void append(int val) {
         x.push_back((val % MAX));
+				memo.push_back({0, (val % MAX)});
+				curIdx = x.size();
     }
     
     void addAll(int inc) {
-        for (int i = 0; i < x.size(); i++) {
-            x[i] = ((x[i] % MAX) + (inc % MAX)) % MAX;
-        }
+				operations.push_back({curIdx, "add", inc});
     }
     
     void multAll(int m) {
-        for (int i = 0; i < x.size(); i++) {
-            int sum = 0;
-            for (int j = 0; j < m; j++) {
-                sum = ((sum % MAX) + x[i]) % MAX;
-            }
-            x[i] = sum;
-        }
+				operations.push_back({curIdx, "mult", m});
     }
     
     int getIndex(int idx) {
         if (x.size() <= idx) return -1;
-        return x[idx];
+				int memoIdx = get<0>(memo[idx]);
+				int result = get<1>(memo[idx]);
+				for (int i = memoIdx; i < operations.size(); i++) {
+					int targetIdx = get<0>(operations[i]);
+					string operation = get<1>(operations[i]);
+					int value = get<2>(operations[i]);
+					if (idx < targetIdx) {
+						if (operation == "add") {
+							result = (int)(((unsigned long)(result % MAX) + (value % MAX)) % MAX);
+						} else if (operation == "mult") {
+							result = (int)(((unsigned long)(result % MAX) * (value % MAX)) % MAX);
+						}
+					}
+				}
+				memo[idx] = {operations.size(), result};
+        return result;
     }
 };
 
